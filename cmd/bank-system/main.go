01b9690f0eb733d8672e2495cc5d5d7f749dfde6
Cristian Guilarte <159936410+CrisGui@users.noreply.github.com>
2024-10-23 16:33:31 +0100

    feat: Add config logic

---
+++ cmd/bank-system/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/CrisGui/bank-system/internal/config"
+
+func main() {
+	config.RunConfig()
+}
---
+++ internal/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+const configPath = "config/default.yaml"
+
+func readConfigFile() map[interface{}]interface{} {
+	file, err := os.ReadFile(configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := make(map[interface{}]interface{})
+
+	err = yaml.Unmarshal(file, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return data
+}
+
+func RunConfig() {
+	fmt.Println("Hello from the config")
+	for k, v := range readConfigFile() {
+		fmt.Printf("%s -> %d\n", k, v)
+	}
+}
